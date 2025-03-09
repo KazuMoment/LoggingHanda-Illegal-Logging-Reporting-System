3163ab9918f0936fd34d47a1edf39cf1e89ac4ec
kebsmirabel <kebinmirabel@gmail.com>
2025-03-09 19:18:06 +0800

    report class

---
+++ report.py
@@ -0,0 +1,48 @@
+from database import get_db_connection
+
+class Report:
+    def __init__(self, location: str, description: str, status = "pending"):
+        self.__location = location  
+        self.__description = description 
+        self._status = status 
+
+    def save_to_db(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO reports (location, description, status) VALUES (?, ?, ?)", 
+            (self.__location, self.__description, self._status)
+        )
+        conn.commit()
+        conn.close()
+        print("Report filed successfully!")
+
+    @staticmethod
+    def get_all_reports():
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, location, status FROM reports")
+        reports = cursor.fetchall()
+        conn.close()
+        return reports
+
+    @staticmethod
+    def get_report_details(report_id: int):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, location, description, status FROM reports WHERE id=?", (report_id,))
+        report = cursor.fetchone()
+        conn.close()
+        return report
+
+    @staticmethod
+    def update_status(report_id: int, status: str):
+        if status not in ["confirmed", "dismissed", "investigated", "pending"]:
+            print("Invalid status!")
+            return
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE reports SET status=? WHERE id=?", (status, report_id))
+        conn.commit()
+        conn.close()
+        print(f"Report {report_id} marked as {status}.")
